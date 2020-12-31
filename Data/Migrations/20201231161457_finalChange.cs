12b92e61db5a800767ca66a092ab73cc46bc4f83
Serap Gülgen <serapgulgen1@gmail.com>
2020-12-31 23:59:39 +0300

    addimage

---
+++ Data/Migrations/20201231161457_finalChange.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Recipes.Data.Migrations
+{
+    public partial class finalChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommentId",
+                table: "Food");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CommentId",
+                table: "Food",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
